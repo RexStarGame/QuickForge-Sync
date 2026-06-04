2f2a57be27664ce6394a0f1a6a7c0569497ddfd9
Patrick <63421519+RexStarGame@users.noreply.github.com>
2026-06-04 23:26:04 +0200

    Fix basic test file

---
+++ QuickForge.Tests/VaultCryptoServiceTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace QuickForge.Tests
+{
+    public class VaultCryptoServiceTests
+    {
+        [Fact]
+        public void TestProject_Works()
+        {
+            Assert.True(true);
+        }
+    }
+}
